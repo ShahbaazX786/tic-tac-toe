27727f16bf40377f824041997fe14169ef2816ab
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-02-19 18:57:45 +0530

    imported new component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { GameboardComponent } from './gameboard/gameboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [GameboardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tic-tac-toe';
