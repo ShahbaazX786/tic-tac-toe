8030687d718065729749c6b40862f29a9db6deb2
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-02-20 22:00:52 +0530

    installed angular material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameboardComponent } from './gameboard/gameboard.component';
 import { SquareComponent } from './square/square.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent, GameboardComponent, SquareComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
