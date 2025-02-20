6cbda402392a0b41001195773866aaf8d67636b2
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-02-20 22:18:02 +0530

    added button module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameboardComponent } from './gameboard/gameboard.component';
 import { SquareComponent } from './square/square.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent, GameboardComponent, SquareComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
