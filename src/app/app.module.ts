4227e92abb2266b04bbaea84dcf682e65a37bd5d
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-02-21 10:43:25 +0530

    Installed nebular theme.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NbLayoutModule, NbThemeModule, NbButtonModule } from '@nebular/theme';
 
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameboardComponent } from './gameboard/gameboard.component';
@@ -8,7 +10,14 @@ import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [AppComponent, GameboardComponent, SquareComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
