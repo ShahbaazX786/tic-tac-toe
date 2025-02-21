999d5ee341e286b02c470ead4fb740645973a29b
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-02-21 10:51:14 +0530

    added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NbLayoutModule, NbThemeModule, NbButtonModule } from '@nebular/theme';
 
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameboardComponent } from './gameboard/gameboard.component';
 import { SquareComponent } from './square/square.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent, GameboardComponent, SquareComponent],
@@ -17,6 +18,12 @@ import { SquareComponent } from './square/square.component';
     NbLayoutModule,
     NbEvaIconsModule,
     NbButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
