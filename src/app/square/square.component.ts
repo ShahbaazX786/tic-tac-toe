0e6f10e76d764bce3928f3a53ca4b3cc6d1c52cf
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-02-22 16:51:50 +0530

    Added gameOver and button disabling logic

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GameStateService } from '../game-state.service';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrl: './square.component.scss',
 })
-export class SquareComponent {
+export class SquareComponent implements OnInit {
   @Input() value!: 'X' | 'O';
+  gameOver: boolean = false;
+  constructor(private gamestate: GameStateService) {}
+
+  ngOnInit(): void {
+    this.gamestate.gameData.subscribe((res) => {
+      if (res.winner.length > res.currentGame) {
+        this.gameOver = true;
+      }
+    });
+  }
 }
